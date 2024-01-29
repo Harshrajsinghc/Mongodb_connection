6c0ba43a06820cf903f57ce2a0f53dfb215e279c
Harshraj Singh Chouhan <chouhanharshrajsingh4@gmail.com>
2024-01-29 15:23:04 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pymongo
+
+# provide the url to connect with the client
+client = pymongo.MongoClient("mongodb+srv://chouhanharshrajsingh4:jaigurudev@cluster0.fnzozwe.mongodb.net/?retryWrites=true&w=majority")
+
+# Database name
+dataBase = client["my_database"]
+
+# Collection name
+collection = dataBase['my_collection']
+
+# data declaration for collection
+d= {'Name':'Raj Verma',
+    'Age': 18,
+    'City':'Indore'}
+
+# insert above record in the collection
+rec = collection.insert_one(d)
+
+# Verify all the records present with all the fields
+all_rec = collection.find()
+
+# printing all the records present in the collection
+for i, record in enumerate(all_rec):
+    print(f"{i}:{record}")
